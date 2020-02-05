cdb074f99680f52d6b2041ab6340b1b33c203393
Eduardo Matos <eduardo.matos.silva@gmail.com>
2020-02-05 20:40:09 -0300

    add graceful shutdown

--- consumer.js
+++ consumer.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid');
 const amqp = require('amqplib');
 const {
   RABBITMQ_QUEUE_NAME,
@@ -13,11 +14,40 @@ async function main() {
   await channel.assertQueue(RABBITMQ_QUEUE_NAME);
   await channel.prefetch(CONCURRENCY);
 
+  const consumerTag = uuid.v4();
+  let messagesBeingProcessed = 0;
+
   console.log('Ready to consume messages...');
   channel.consume(RABBITMQ_QUEUE_NAME, async (rawMsg) => {
+    messagesBeingProcessed += 1;
+
     await sleep(random(MIN_DELAY, MAX_DELAY)); // simulate slow process
     channel.ack(rawMsg);
     console.log(`Processed message "${rawMsg.content.toString()}"!`);
+
+    messagesBeingProcessed -= 1;
+  }, { consumerTag });
+
+  process.on('SIGTERM', async () => {
+    console.log('Gracefully shutting down...');
+    console.log('Stopping to receive new messages...');
+    await channel.cancel(consumerTag);
+
+    while(true) {
+      if (messagesBeingProcessed === 0) {
+        console.log('All messages have been processed. Proceeding with shutdown...');
+        break;
+      }
+
+      console.log('Waiting all messages to be processed before shutting down...');
+      await sleep(1000);
+    }
+
+    console.log('Closing channel and connection...');
+    await channel.close();
+    await conn.close();
+
+    console.log('Shutting down!');
   });
 }
 
